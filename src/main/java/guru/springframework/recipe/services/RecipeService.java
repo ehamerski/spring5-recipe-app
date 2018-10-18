141140476858ae5d05ffa165993755b2f3c5dfce
Eduardo Hamerski <ehamerski@gmail.com>
2018-10-17 23:03:37 -0300

    Added Recipe Form and the functionality for insert, update and delete recipes. Updated Recipe List at index page to show the new actions. Implemented all the unit tests to cover this section.

--- src/main/java/guru/springframework/recipe/services/RecipeService.java
+++ src/main/java/guru/springframework/recipe/services/RecipeService.java
@@ -3,11 +3,12 @@ package guru.springframework.recipe.services;
 import guru.springframework.recipe.commands.RecipeCommand;
 import guru.springframework.recipe.domain.Recipe;
 
-import java.util.Optional;
 import java.util.Set;
 
 public interface RecipeService {
     Set<Recipe> getRecipeList();
     Recipe findById(Long id);
+    RecipeCommand findCommandById(Long id);
     RecipeCommand saveRecipeCommand(RecipeCommand command);
+    void deleteById(Long id);
 }
